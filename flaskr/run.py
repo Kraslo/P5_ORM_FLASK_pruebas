a3c96752841193b55cfa853d548ee1b7670139c7
Alejandro Pozo Huertas <apozohue10@gmail.com>
2023-04-18 18:07:56 +0200

    Update run.py

--- flaskr/run.py
+++ flaskr/run.py
@@ -7,6 +7,7 @@ import os
 import json
 
 app = Flask(__name__)
+app.app_context().push()
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 app.config['SQLALCHEMY_DATABASE_URI'] = ### Definir la URI de Conexión
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
